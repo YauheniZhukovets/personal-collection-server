094aa3d448ed55854e0f86bdc2a93ba6c3b4f2f2
YauheniZhukovets <stewart-8@mail.ru>
2023-02-05 23:29:34 +0400

    added fields in res.cookie(...)

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,12 +6,17 @@ class UserController {
     async registration(req, res, next) {
         try {
             const errors = validationResult(req)
-            if(!errors.isEmpty()) {
+            if (!errors.isEmpty()) {
                 return next(ApiError.BadRequest('Ошибка валидации', errors.array()))
             }
             const {email, password} = req.body
             const userData = await userService.registration(email, password)
-            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60* 60 * 1000, httpOnly: true})
+            res.cookie('refreshToken', userData.refreshToken, {
+                maxAge: 30 * 24 * 60 * 60 * 1000,
+                sameSite: 'none',
+                secure: true,
+                httpOnly: true
+            })
             return res.json(userData)
         } catch (e) {
             next(e)
@@ -22,7 +27,12 @@ class UserController {
         try {
             const {email, password} = req.body
             const userData = await userService.login(email, password)
-            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60* 60 * 1000, httpOnly: true})
+            res.cookie('refreshToken', userData.refreshToken, {
+                maxAge: 30 * 24 * 60 * 60 * 1000,
+                sameSite: 'none',
+                secure: true,
+                httpOnly: true
+            })
             return res.json(userData)
         } catch (e) {
             next(e)
@@ -44,7 +54,12 @@ class UserController {
         try {
             const {refreshToken} = req.cookies
             const userData = await userService.refresh(refreshToken)
-            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60* 60 * 1000, httpOnly: true})
+            res.cookie('refreshToken', userData.refreshToken, {
+                maxAge: 30 * 24 * 60 * 60 * 1000,
+                sameSite: 'none',
+                secure: true,
+                httpOnly: true
+            })
             return res.json(userData)
         } catch (e) {
             next(e)
