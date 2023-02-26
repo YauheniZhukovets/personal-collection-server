8ccb23312d4b43e6c5eae28f1fd6800da550bf75
YauheniZhukovets <stewart-8@mail.ru>
2023-02-26 12:17:04 +0400

    fix: added count into tag

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use(express.json())
 app.use(cors({
     credentials: true,
     origin: true
-    //origin: process.env.CLIENT_URL
 }))
 app.use(cookieParser())
 app.use('/api', router)
--- service/tag-service.js
+++ service/tag-service.js
@@ -1,8 +1,17 @@
 const TagModel = require('../models/tag-model')
+const ItemModel = require('../models/item-model')
 
 class TagService {
-    getTags() {
-        return TagModel.find({})
+    async getTags() {
+        const tags = await TagModel.find({})
+
+        const finishTags = []
+        for (const tag of tags) {
+            await ItemModel.count({tags: {$all: tag.title}})
+                .exec()
+                .then((count) => finishTags.push({title: tag.title, count}))
+        }
+        return finishTags.filter(t => t.count > 1)
     }
 }
 
