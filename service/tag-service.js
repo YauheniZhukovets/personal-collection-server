2d0a981880c31ec1a1ea346ac229a9b22edfe8f5
YauheniZhukovets <stewart-8@mail.ru>
2023-02-26 12:20:24 +0400

    fix: added count into tag

--- service/tag-service.js
+++ service/tag-service.js
@@ -9,7 +9,7 @@ class TagService {
         for (const tag of tags) {
             await ItemModel.count({tags: {$all: tag.title}})
                 .exec()
-                .then((count) => finishTags.push({title: tag.title, count}))
+                .then((count) => finishTags.push({_id: tag._id, title: tag.title, count}))
         }
         return finishTags.filter(t => t.count > 0)
     }
